c3545d7838ce8b6fe3975548c37d7edb49de93fd
c332030 <c332030@c332030.com>
2023-04-13 10:51:22 +0800

    feat: init.bat

---
+++ nvm/init.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set NVM_HOME="%userprofile%\AppData\Roaming\nvm"
+
+setx /M NVM_HOME "%NVM_HOME%"
+setx /M NVM_SYMLINK "C:\Program Files\nodejs"
+
+call link-dir "%~dp0app" "%NVM_HOME%"
+
+call link-dir "%~dp0data\prefix" "%~dp0app\prefix"
+
+call link-dir "%~dp0data\cache" "%userprofile%\AppData\Local\npm-cache"
+
+call link-file "%~dp0data" "%userprofile%" ".npmrc"
+
+pause >nul
