411d06e8d0d7a37c34025846f113fbaf90dd6f05
c332030 <c332030@c332030.com>
2026-03-26 13:43:14 +0800

    feat: link

--- config/link.bat
+++ config/link.bat
@@ -2,12 +2,13 @@
 
 :: setx /m GIT_HOME %~dp0app
 
-set DATA_PATH=%~dp0data
+set CURRENT_DIR=%~dp0
 
 call link-file %DATA_PATH% %userprofile% npmrc .npmrc
 
-set PNPM_CONFIG=%userprofile%\AppData\Local\pnpm\config
-mkdir %PNPM_CONFIG%
-call link-dir %DATA_PATH% %PNPM_CONFIG%
+PNPM_HOME=%userprofile%\AppData\Local\pnpm
+
+call link-dir %CURRENT_DIR%\config %PNPM_HOME%\config
+call link-dir %CURRENT_DIR%\store %PNPM_HOME%\store
 
 pause
